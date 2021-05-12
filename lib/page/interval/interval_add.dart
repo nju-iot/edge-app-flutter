7cc3d95ea1d964ad88c205cf1be3537e6c166f2e
Tzung-Lin <3407584434@qq.com>
2021-05-12 11:39:41 +0800

    删除日期选择器，改为自行实现

--- lib/page/interval/interval_add.dart
+++ lib/page/interval/interval_add.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
 class IntervalAddPage extends StatefulWidget {
   @override
   _IntervalAddPageState createState() => _IntervalAddPageState();
 }
 
 class _IntervalAddPageState extends State<IntervalAddPage> {
-  bool _runOnce=false;
+  bool _runOnce=false;//只运行一次
+  DateTime _startDateTime;//开始时间
+  DateTime _endDateTime;//结束时间
+  
   //提交表单
   void _formSubmit(){
     print("提交表单");
   }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDateTime=DateTime.now();
+  }
+
+  
+  
+  Future<DateTime> _showDatePicker(){
+    _startDateTime=DateTime.now();
+    return showCupertinoModalPopup(
+      context: context , 
+      builder: (ctx){
+        return SizedBox(
+          height: 200,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.dateAndTime,
+            initialDateTime: _startDateTime,
+            onDateTimeChanged: (DateTime selectedDate){
+              print(selectedDate);
+            },
+            ),
+        );
+      },
+      );
+  }
+  
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +56,7 @@ class _IntervalAddPageState extends State<IntervalAddPage> {
       child: Text("提交"),),
       body: Form(
         child: Column(
+          
           children: <Widget>[
             TextFormField(
               decoration: InputDecoration(
@@ -27,6 +64,17 @@ class _IntervalAddPageState extends State<IntervalAddPage> {
                 labelStyle: TextStyle(color: Colors.black),
                 hintText: "请输入名称",
                 errorText: "此为必填项",
+              ),
+            ),
+            TextFormField(
+              onTap: () async {
+                
+              },
+              initialValue: _startDateTime.toString(),
+              enabled: false,
+              decoration: InputDecoration(
+                labelText: "开始时间",
+                labelStyle: TextStyle(color: Colors.black),
               ),
             ),
             Switch(
@@ -46,6 +94,7 @@ class _IntervalAddPageState extends State<IntervalAddPage> {
                   contentPadding: EdgeInsets.symmetric(horizontal: 100.0),
                   
                 ),
+                
               ),
             
           ],
