61d754d985bb58736998fef3a19a92c142bc5fe9
TzungLin <3407584434@qq.com>
2021-06-16 15:55:11 +0800

    修复任务行动列表无法显示超出荧幕个数项目的问题

--- lib/page/interval/interval_actions.dart
+++ lib/page/interval/interval_actions.dart
@@ -154,179 +154,182 @@ class IntervalActionsPageState extends State<IntervalActionsPage> {
               ],
             ),
           ),
-          body: Column(
-            children: [
-              ListTile(
-                title: const Text("任务行动"),
-                trailing: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.2,
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () async {
-                          bool confirmDelete = false;
-                          confirmDelete = await showDialog<bool>(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("提示"),
-                                  content: Text("确定要删除选中定时任务吗?"),
-                                  actions: <Widget>[
-                                    FlatButton(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text("任务行动"),
+                  trailing: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () async {
+                            bool confirmDelete = false;
+                            confirmDelete = await showDialog<bool>(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("提示"),
+                                    content: Text("确定要删除选中定时任务吗?"),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(false),
+                                          child: Text("取消")),
+                                      FlatButton(
                                         onPressed: () =>
-                                            Navigator.of(context).pop(false),
-                                        child: Text("取消")),
-                                    FlatButton(
-                                      onPressed: () =>
-                                          Navigator.of(context).pop(true),
-                                      child: Text("确认"),
-                                    ),
-                                  ],
-                                );
-                              });
-                          if (confirmDelete) {
-                            for (String actionId in _selectedToDelete) {
-                              await MyHttp.delete(
-                                      ':48085/api/v1/intervalaction/${actionId}')
-                                  .catchError((error) {
-                                print(error);
-                                print(error.response);
-                                showDialog<bool>(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return AlertDialog(
-                                        title: const Text("错误提示"),
-                                        content:
-                                            Text(error.response.toString()),
-                                        actions: <Widget>[
-                                          FlatButton(
-                                              onPressed: () =>
-                                                  Navigator.of(context)
-                                                      .pop(true),
-                                              child: Text("确认")),
-                                        ],
-                                      );
-                                    });
-                              });
-                            }
+                                            Navigator.of(context).pop(true),
+                                        child: Text("确认"),
+                                      ),
+                                    ],
+                                  );
+                                });
+                            if (confirmDelete) {
+                              for (String actionId in _selectedToDelete) {
+                                await MyHttp.delete(
+                                        ':48085/api/v1/intervalaction/${actionId}')
+                                    .catchError((error) {
+                                  print(error);
+                                  print(error.response);
+                                  showDialog<bool>(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: const Text("错误提示"),
+                                          content:
+                                              Text(error.response.toString()),
+                                          actions: <Widget>[
+                                            FlatButton(
+                                                onPressed: () =>
+                                                    Navigator.of(context)
+                                                        .pop(true),
+                                                child: Text("确认")),
+                                          ],
+                                        );
+                                      });
+                                });
+                              }
 
-                            List<Map<String, dynamic>> data =
-                                await _getActionList();
-                            _streamController.sink.add(data);
-
-                            _selectedToDelete.clear();
-                          }
-                        },
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.add),
-                          onPressed: () {
-                            MyRouter.pushAndDo(Routes.intervalActionsAddPage,
-                                (_) async {
                               List<Map<String, dynamic>> data =
                                   await _getActionList();
                               _streamController.sink.add(data);
-                            });
+
+                              _selectedToDelete.clear();
+                            }
                           },
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              StreamBuilder(
-                stream: _streamController.stream,
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.hasData) {
-                    if (snapshot.data.length == 0) {
-                      return Text("未搜索到符合的结果，请修改搜索条件或新增项目");
-                    }
-                    return Expanded(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Material(
-                          elevation: 4.0,
-                          child: Container(
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: snapshot.data.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                return Card(
-                                    child: ListTile(
-                                  leading: Checkbox(
-                                    onChanged: (isSelected) {
-                                      if (isSelected) {
-                                        _selectedToDelete.add(snapshot
-                                            .data[index]["id"]
-                                            .toString());
-                                      } else {
-                                        _selectedToDelete.remove(snapshot
-                                            .data[index]['id']
-                                            .toString());
-                                      }
-                                      setState(() {});
-                                    },
-                                    value: _selectedToDelete.contains(
-                                        snapshot.data[index]['id'].toString()),
-                                  ),
-                                  title: Text(
-                                    "${snapshot.data[index]['name']}",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  subtitle: Text(
-                                    "id: ${snapshot.data[index]['id']}",
-                                    overflow: TextOverflow.fade,
-                                    softWrap: false,
-                                  ),
-                                  trailing: IconButton(
-                                    icon: Icon(
-                                      Icons.arrow_forward_ios,
-                                    ),
-                                    onPressed: () {
-                                      //TODO: 跳转到行动修改页面
-                                      print(snapshot.data[index]);
-                                      Map<String, dynamic> m =
-                                          snapshot.data[index];
-                                      m['path'] =
-                                          m['path'].replaceAll('/', '.');
-
-                                      MyRouter.pushAndDo(
-                                          Routes.actionInfoPage(
-                                              actionInfo: jsonEncode(m)),
-                                          (_) async {
-                                        List<Map<String, dynamic>> data =
-                                            await _getActionList();
-                                        _streamController.sink.add(data);
-                                      });
-                                    },
-                                  ),
-                                ));
-                              },
-                            ),
+                        Expanded(
+                          child: IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {
+                              MyRouter.pushAndDo(Routes.intervalActionsAddPage,
+                                  (_) async {
+                                List<Map<String, dynamic>> data =
+                                    await _getActionList();
+                                _streamController.sink.add(data);
+                              });
+                            },
                           ),
                         ),
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    print("Error: ${snapshot.error}");
-                    return Text(
-                      "请求失败，请刷新页面或检查网络连接情况",
-                      style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
-                    );
-                  } else {
-                    return SizedBox(
-                        height: 300,
-                        child: Center(child: CircularProgressIndicator()));
-                  }
-                },
-              ),
-            ],
+                      ],
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: StreamBuilder(
+                    stream: _streamController.stream,
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data.length == 0) {
+                          return Text("未搜索到符合的结果，请修改搜索条件或新增项目");
+                        }
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Material(
+                            elevation: 4.0,
+                            child: Container(
+                              child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Card(
+                                      child: ListTile(
+                                    leading: Checkbox(
+                                      onChanged: (isSelected) {
+                                        if (isSelected) {
+                                          _selectedToDelete.add(snapshot
+                                              .data[index]["id"]
+                                              .toString());
+                                        } else {
+                                          _selectedToDelete.remove(snapshot
+                                              .data[index]['id']
+                                              .toString());
+                                        }
+                                        setState(() {});
+                                      },
+                                      value: _selectedToDelete.contains(snapshot
+                                          .data[index]['id']
+                                          .toString()),
+                                    ),
+                                    title: Text(
+                                      "${snapshot.data[index]['name']}",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    subtitle: Text(
+                                      "id: ${snapshot.data[index]['id']}",
+                                      overflow: TextOverflow.fade,
+                                      softWrap: false,
+                                    ),
+                                    trailing: IconButton(
+                                      icon: Icon(
+                                        Icons.arrow_forward_ios,
+                                      ),
+                                      onPressed: () {
+                                        //TODO: 跳转到行动修改页面
+                                        print(snapshot.data[index]);
+                                        Map<String, dynamic> m =
+                                            snapshot.data[index];
+                                        m['path'] =
+                                            m['path'].replaceAll('/', '.');
+
+                                        MyRouter.pushAndDo(
+                                            Routes.actionInfoPage(
+                                                actionInfo: jsonEncode(m)),
+                                            (_) async {
+                                          List<Map<String, dynamic>> data =
+                                              await _getActionList();
+                                          _streamController.sink.add(data);
+                                        });
+                                      },
+                                    ),
+                                  ));
+                                },
+                              ),
+                            ),
+                          ),
+                        );
+                      } else if (snapshot.hasError) {
+                        print("Error: ${snapshot.error}");
+                        return Text(
+                          "请求失败，请刷新页面或检查网络连接情况",
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
+                        );
+                      } else {
+                        return SizedBox(
+                            height: 300,
+                            child: Center(child: CircularProgressIndicator()));
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
           )),
       onRefresh: () async {
         List<Map<String, dynamic>> data = await _getActionList();
